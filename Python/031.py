56bf2b9c0a82c17a140f3e295ffad662eba45129
Nalu <pordosol699@gmail.com>
2025-11-07 11:38:18 -0300

    exercicio 31

---
+++ Python/031.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número e alerte quando o mesmo for maior do que 100.
+
+while True:
+    num=int(input())
+    if(num>100):
+        print("Número maior que 100")
+        break
